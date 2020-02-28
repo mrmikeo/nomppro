74c5c3560c386e54591d1c35a44adec5f7ba7b0a
MikeO <10thmountain@zoho.com>
2020-02-28 11:46:13 +0800

    Update stats.js

--- libs/stats.js
+++ libs/stats.js
@@ -8,6 +8,39 @@ var os = require('os');
 
 var algos = require('stratum-pool/lib/algoProperties.js');
 
+/**
+ * Sort object properties (only own properties will be sorted).
+ * @param {object} obj object to sort properties
+ * @param {string|int} sortedBy 1 - sort object properties by specific value.
+ * @param {bool} isNumericSort true - sort object properties as numeric value, false - sort as string value.
+ * @param {bool} reverse false - reverse sorting.
+ * @returns {Array} array of items in [[key,value],[key,value],...] format.
+ */
+function sortProperties(obj, sortedBy, isNumericSort, reverse) {
+    sortedBy = sortedBy || 1; // by default first key
+    isNumericSort = isNumericSort || false; // by default text sort
+    reverse = reverse || false; // by default no reverse
+
+    var reversed = (reverse) ? -1 : 1;
+
+    var sortable = [];
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            sortable.push([key, obj[key]]);
+        }
+    }
+    if (isNumericSort)
+        sortable.sort(function (a, b) {
+            return reversed * (a[1][sortedBy] - b[1][sortedBy]);
+        });
+    else
+        sortable.sort(function (a, b) {
+            var x = a[1][sortedBy].toLowerCase(),
+                y = b[1][sortedBy].toLowerCase();
+            return x < y ? reversed * -1 : x > y ? reversed : 0;
+        });
+    return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+}
 
 module.exports = function(logger, portalConfig, poolConfigs){
 
@@ -173,6 +206,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
                         };
                         allCoinStats[coinStats.name] = (coinStats);
                     }
+		    allCoinStats = sortPoolsByName(allCoinStats);
                     callback();
                 }
             });
@@ -288,6 +322,17 @@ module.exports = function(logger, portalConfig, poolConfigs){
 
     };
 
+    function sortPoolsByName(objects) {
+        var newObject = {};
+        var sortedArray = sortProperties(objects, 'name', false, false);
+        for (var i = 0; i < sortedArray.length; i++) {
+            var key = sortedArray[i][0];
+            var value = sortedArray[i][1];
+            newObject[key] = value;
+        }
+        return newObject;
+    }
+	
     this.getReadableHashRateString = function(hashrate){
         var i = -1;
         var byteUnits = [ ' KH', ' MH', ' GH', ' TH', ' PH' ];
